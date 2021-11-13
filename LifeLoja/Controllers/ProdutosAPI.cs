71a26381e3f57a3dafe610772d4721cf4d3316df
João Paulo dos Santos Santana <santanajps@taginfraestrutura.com.br>
2021-11-13 10:45:19 -0300

    testes

---
+++ LifeLoja/Controllers/ProdutosAPI.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LifeLoja.Entidades;
+using LifeLoja.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeLoja.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosAPI : ControllerBase
+    {
+        private readonly ProdutosService _produtoRepository;
+
+        public ProdutosAPI(ProdutosService produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Produtos>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Produtos>>> GetProducts()
+        {
+            var products = await _produtoRepository.GetProduto();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Produtos), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Produtos>> GetProductById(string id)
+        {
+            var product = await _produtoRepository.GetProdut(id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Produtos>))]
+        public async Task<ActionResult<IEnumerable<Produtos>>> GetProductByCategory(string category)
+        {
+            if (category is null)
+            {
+                return BadRequest("Invalid category");
+            }
+
+            var products = await _produtoRepository.GetProductByCategory(category);
+
+            return Ok(products);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Produtos), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Produtos>> CreateProduto([FromBody] Produtos produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest("Produto Invalido");
+            }
+
+            await _produtoRepository.CreateProduct(produto);
+
+            return CreatedAtRoute("GetProduct", new { id = produto.Id }, produto);
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Produtos), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdateProduto([FromBody] Produtos produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest("Produto invalido");
+            }
+
+            return Ok(await _produtoRepository.UpdateProduct(produto));
+        }
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Produtos), StatusCodes.Status200OK)]
+
+        public async Task<IActionResult> DeleteProductId(string id)
+        {
+            return Ok(await _produtoRepository.DeleteProduct(id));
+        }
+
+    }
+}
---
+++ LifeLoja/Services/ProdutosService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LifeLoja.DatabaseSettings;
+using LifeLoja.Entidades;
+using MongoDB.Driver;
+
+namespace LifeLoja.Services
+{
+    public class ProdutosService 
+    {
+
+        private readonly IMongoCollection<Produtos> _context;
+
+        public ProdutosService(ProdutoStoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _context = database.GetCollection<Produtos>(settings.ProdutosCollectionName);
+        }
+
+
+        public async Task CreateProduct(Produtos product)
+        {
+            await _context.InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Produtos> filter = Builders<Produtos>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await _context.DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+
+
+        public async Task<IEnumerable<Produtos>> GetProductByCategory(string categoryName)
+        {
+            FilterDefinition<Produtos> filter = Builders<Produtos>.Filter.Eq(p => p.Category, categoryName);
+
+            return await _context.Find(filter).ToListAsync();
+        }
+
+        public async Task<Produtos> GetProdut(string id)
+        {
+            return await _context.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Produtos>> GetProduto()
+        {
+            return await _context.Find(p => true).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Produtos>> GetProdutoByName(string name)
+        {
+            FilterDefinition<Produtos> filter = Builders<Produtos>.Filter.ElemMatch(p => p.Name, name);
+
+            return await _context.Find(filter).ToListAsync();
+        }
+
+        public async Task<bool> UpdateProduct(Produtos product)
+        {
+            var updateResult = await _context.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+
+    }
+}
