09a65007251d40fa7015d0b056b06161744f114c
santanajps <santanajps@taginfraestrutura.com.br>
2021-11-15 22:57:21 -0300

    testes falta separar por pastas ou implementar

--- LojaLife.Tests/LifeTestes.cs
+++ LojaLife.Tests/LifeTestes.cs
@@ -1,9 +1,9 @@
-using System.Collections.Generic;
+ï»¿using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LifeLoja.Controllers;
-using LifeLoja.Services;
 using LifeLoja.Entidades;
+using LifeLoja.Services;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using ProdutoFakeData.ProdutosViewFake.ProdutosData;
@@ -18,6 +18,7 @@ namespace LojaLife.Tests
         private readonly ProdutosAPI _produtosAPI;
         private readonly ProdutosDataFake _produtosDataFake;
         private readonly List<ProdutosDataFake> _ListProdutosDataFake;
+        private readonly List<Produtos> prod;
 
         public LifeTestes()
         {
@@ -25,6 +26,13 @@ namespace LojaLife.Tests
             _produtosAPI = new ProdutosAPI(_produtosservice);
             _produtosDataFake = new ProdutosViewFaker().Generate();
             _ListProdutosDataFake = new ProdutosViewFaker().Generate(50);
+
+           
+
+            prod.Add(new Produtos("6192fcf6172b8d4f097c101a", "Camisa Oakley", "Camisas", "Verde Azul", 29, 30));
+            prod.Add(new Produtos("61930077d3f63948cf05ad75", "Camisa Hurley", "Camisas", "Preto e Branco", 70, 30));
+            prod.Add(new Produtos("61930099cf9694b84786b192", "Regata Nike", "Regatas", "Vermelho Listrada", 100, 20));
+            prod.Add(new Produtos("619300c1a22a2a591c21bcb7", "BlusÄƒo Champgion", "BlusÄƒo", "Preto", 200, 20));
 
 
         }
@@ -34,16 +42,26 @@ namespace LojaLife.Tests
         [Fact]
         public async Task Get_Ok()
         {
-            List<Produtos> prod = new List<Produtos>();
-
-            prod.Add(new Produtos("6192fcf6172b8d4f097c101a","Camisa Oakley","Camisas","Verde Azul",29,30));
-            prod.Add(new Produtos("61930077d3f63948cf05ad75", "Camisa Hurley", "Camisas", "Preto e Branco", 70, 30));
-            prod.Add(new Produtos("61930099cf9694b84786b192", "Regata Nike", "Regatas", "Vermelho Listrada", 100, 20));
-            prod.Add(new Produtos("619300c1a22a2a591c21bcb7", "Blusão Champgion", "Blusão", "Preto", 200, 20));
-            var buscar =  await _produtosAPI.GetProducts();
-
            
+
+          
+              _produtosservice.GetProduto().Returns(prod);
+
         }
+
+
+
+        [Fact]
+        public async Task GetId_Ok()
+        {
+            _produtosservice.GetProduto().Returns(prod);
+
+
+
+
+        }
+
+
 
     }
 }
